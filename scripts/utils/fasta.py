ef2b8dd654fc1d8a9c9b27ecdcb3483c5ba74331
kiesers <silas.kieser@gmail.com>
2020-01-29 12:50:01 +0100

    pure python fasta split don't equilibrate

--- scripts/utils/fasta.py
+++ scripts/utils/fasta.py
@@ -1,5 +1,3 @@
-from Bio import SeqIO
-from numpy import ceil
 import os
 
 def _make_test_fasta(test_file='test_ABC.fasta'):
@@ -9,7 +7,6 @@ def _make_test_fasta(test_file='test_ABC.fasta'):
 
 def str2multiline(long_string,line_length=80):
     return [long_string[i:i+line_length]   for i in range(0,len(long_string),line_length)]
-
 
 def count_Nseq(fasta_file):
     """
@@ -28,51 +25,46 @@ def count_Nseq(fasta_file):
                 i+=1
     return i
 
-
-def split(fasta_file,maxSubsetSize,out_dir, simplify_headers=True):
+def split(fasta_file,SubsetSize,out_dir):
     """
     Splits a fasta in subsets of size max maxSubsetSize.
     >>> fasta_file='test_ABC.fasta'
     >>> out_dir = 'test_outdit_doctest'
     >>> _make_test_fasta(fasta_file) # makes fasta with a seq for each nucleotide
-    >>> split(fasta_file,3,out_dir,simplify_headers=True)
+    >>> split(fasta_file,3,out_dir)
     >>> len(os.listdir(out_dir))
     2
     >>> count_Nseq('test_outdit_doctest/subset1.fasta')
-    2
+    3
     >>> count_Nseq('test_outdit_doctest/subset2.fasta')
-    2
-    >>> split(fasta_file,3,out_dir,simplify_headers=True)
+    1
+    >>> split(fasta_file,3,out_dir)
     Traceback (most recent call last):
         ...
     FileExistsError: [Errno 17] File exists: 'test_outdit_doctest'
     >>> import shutil; shutil.rmtree(out_dir)
     >>> os.remove(fasta_file)
     """
-
-    N= count_Nseq(fasta_file)
-
-    SubsetSize= int(ceil( N/ceil(N/ maxSubsetSize)))
     extension= os.path.splitext(fasta_file)[-1]
-
     os.makedirs(out_dir)
+
 
     i,subset_n=0,0
     fout= None
-    for i,seq in enumerate(SeqIO.parse(fasta_file,'fasta')):
-        if (i % SubsetSize) == 0:
-            subset_n+=1
-            if fout is not None:
-                fout.close()
 
-            fout = open(f"{out_dir}/subset{subset_n}{extension}",'w')
+    with open(fasta_file,'r') as fin:
+        for line in fin:
 
-        if simplify_headers:
-            seq.description=''
-        SeqIO.write(seq,fout,'fasta')
+            if line[0]=='>':
 
-    fout.close()
+                if (i % SubsetSize) == 0:
+                    subset_n+=1
+                    if fout is not None:
+                        fout.close()
+                    fout = open(f"{out_dir}/subset{subset_n}{extension}",'w')
 
+                i+=1
+            fout.write(line)
 
 def header2origin(fasta_file,out,simplify_header=True):
     """
